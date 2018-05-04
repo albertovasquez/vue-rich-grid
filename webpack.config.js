92e0472ec5152a1fe77f35ea1a3d70a5937eac4a
Alberto Vasquez <alberto.vasquez@gmail.com>
2018-05-04 00:37:06 -0400

    Ensure when required async await is allowed and runtimes are included

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const { VueLoaderPlugin } = require('vue-loader');
 const path = require('path')
 const webpack = require('webpack')
+require("babel-polyfill");
 
 module.exports = {
-  entry: './src/main.js',
+  entry: ["babel-polyfill", './src/main.js'],
   plugins: [
     new VueLoaderPlugin()
   ],
@@ -41,22 +42,22 @@ module.exports = {
   },
 }
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
-}
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports.devtool = '#source-map'
+//   module.exports.plugins = (module.exports.plugins || []).concat([
+//     new webpack.DefinePlugin({
+//       'process.env': {
+//         NODE_ENV: '"production"'
+//       }
+//     }),
+//     new webpack.optimize.UglifyJsPlugin({
+//       sourceMap: true,
+//       compress: {
+//         warnings: false
+//       }
+//     }),
+//     new webpack.LoaderOptionsPlugin({
+//       minimize: true
+//     })
+//   ])
+// }
