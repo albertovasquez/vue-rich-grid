6b210fedb2f5e594f83fe9febb399730060528fb
Alberto Vasquez <alberto.vasquez@gmail.com>
2018-06-22 00:32:20 -0400

    start testing empty base params

--- test/VueRichGrid.spec.js
+++ test/VueRichGrid.spec.js
@@ -2,9 +2,9 @@ import { shallowMount } from '@vue/test-utils'
 import VueRichGrid from '@/components/VueRichGrid.vue'
 
 describe('VueRichGrid', () => {
-  it('handles empty props for baseParams', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(VueRichGrid, {})
-    // expect(true).toBe.true();
-  })
+    describe('handles empty params', () => {
+        it('for baseParams', () => {
+            expect(shallowMount(VueRichGrid, {}).isVueInstance()).toBe.true;
+        })
+    })
 })
